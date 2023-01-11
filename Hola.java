585279be30ac4caf37eb230393d38aa183ea60f4
Juan Carrasquer <juancarrasquer@gmail.com>
2023-01-12 00:32:08 +0100

    Añadida una linea a hola.java

--- Hola.java
+++ Hola.java
@@ -4,5 +4,7 @@ public class Hola
     public static void public static void main(String[] args) 
     {
         System.out.println("Hola mundo");
+
+        System.out.println("Adios mundo");
     }
 }
